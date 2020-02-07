8b3838ea1e4207b1ad830152cb7b57df8a7d9ba0
iavinashkumar <51225993+iavinashkumar@users.noreply.github.com>
2020-02-07 12:34:01 +0530

    Update main.c
    done

--- src/main.c
+++ src/main.c
@@ -5,4 +5,7 @@ int main()
     int x =100;
     printf("hello world and value of x is %d \n",x);
     printf(">>hello world and value of x is %d \n",x);
+    printf(">>hello world and value of x is %d \n",x);
+    printf(">>hello world and value of x is %d \n",x);
+
 }
